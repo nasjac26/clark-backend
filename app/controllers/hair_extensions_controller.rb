605593922c8c5cd68bccea870f3458a9e9d47201
Jacob <Jacobonasrallah@gmail.com>
2022-01-27 14:52:31 -0800

    added updating stock from admin panel

--- app/controllers/hair_extensions_controller.rb
+++ app/controllers/hair_extensions_controller.rb
@@ -16,9 +16,19 @@ class HairExtensionsController < ApplicationController
     # def edit
     # end
     
-    # def update
-    # end
+    def update
+        this_product = HairExtension.find(params[:id])
+        this_product.update!(product_update_params)
+        render json: this_product, status: :ok
+
+    end
     
     # def delete
     # end
+
+    private
+
+    def product_update_params
+        params.permit(:stock, :id, :hair_extension)
+    end
 end
