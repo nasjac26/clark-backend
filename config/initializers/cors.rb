b01ba135d484ef2eebec93e4e98285f8b78b5b73
Jacob <Jacobonasrallah@gmail.com>
2022-03-31 15:09:58 -0700

    production check on phanes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://clarks-backend.herokuapp.com'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :partch, :delete, :options, :head]
   
     end
   end
