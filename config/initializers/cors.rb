2b04df5f60ef44ff12b4481f0084c718d833de2f
Jacob <Jacobonasrallah@gmail.com>
2022-03-31 15:14:52 -0700

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://clarks-backend.herokuapp.com'
+    origins 'https://clark-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
